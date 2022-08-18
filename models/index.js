9f93645ddbc0868b98d7c11298487a52fdd0f046
Mateo <mateom.realtor@gmail.com>
2022-08-18 13:38:12 -0400

    created relatioships

--- models/index.js
+++ models/index.js
@@ -5,26 +5,41 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+Category.belongsTo(Product)
+
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+});
 Product.belongsTo(Category)
 
 
 
-
-// Categories have many Products
-Category.hasMany(Product)
-
-
-
-
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(ProductTag)
-
-
-
+Product.belongsToMany(ProductTag, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+  through:"Product"
+});
+Product.belongsTo(ProductTag,{
+  through:"Product"
+})
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product)
-
+Tag.belongsToMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+  through: "TAG"
+});
+Tag.belongsTo(Product, {
+  through:" TAG"
+})
 
 
 
